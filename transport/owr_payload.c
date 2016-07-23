c557e3acb0a53ba2dbfe4202a22d9c3202b611fe
Yegor Alexeyev <yegor.alexeyev@gmail.com>
2016-07-23 19:13:58 +0300

    Revert "pli in rtp caps"
    This reverts commit e739e26d975665e6bc66de86ff41eb45e14c02a3.

--- transport/owr_payload.c
+++ transport/owr_payload.c
@@ -615,7 +615,6 @@ GstCaps * _owr_payload_create_rtp_caps(OwrPayload *payload)
         "encoding-name", G_TYPE_STRING, encoding_name,
         "payload", G_TYPE_INT, priv->payload_type,
         "clock-rate", G_TYPE_INT, priv->clock_rate,
-        "rtcp-fb-nack-pli", G_TYPE_INT, 1,
         NULL);
 
     g_object_get(payload, "media-type", &media_type, NULL);
