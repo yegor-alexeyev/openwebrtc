e3269a83ae2cac37aeb955a94ec74f1f56720c10
Yegor Alexeyev <yegor.alexeyev@gmail.com>
2016-07-24 16:49:33 +0300

    Revert "make C compiler happy"
    This reverts commit 481e3cd51638134c5bd48184d5a9ca68272f4d9a.

--- local/owr_local_media_source.c
+++ local/owr_local_media_source.c
@@ -447,14 +447,11 @@ static void cb_new_rtspsrc_pad(GstElement *element,GstPad*pad,gpointer  data)
 }
 
 gboolean
-cb_select_stream (void *rtspsrc,
-               guint       num,
+cb_select_stream (void *,
+               guint       ,
                GstCaps    *caps,
-               gpointer    user_data)
+               gpointer    )
 {
-  (void)(rtspsrc);
-  (void)(num);
-  (void)(user_data);
   gst_caps_set_simple(caps, "rtcp-fb-nack-pli", G_TYPE_INT, 1, NULL);
 }
 
