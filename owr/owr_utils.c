d8448a5318ab7fc6923748b5fee7c5c89970b8ae
Yegor Alexeyev <yegor.alexeyev@gmail.com>
2016-07-24 16:51:36 +0300

    Revert "debug log"
    This reverts commit 255616fc0e82864423c98ec3f95435b8209411f7.

--- owr/owr_utils.c
+++ owr/owr_utils.c
@@ -195,9 +195,6 @@ void _owr_deep_notify(GObject *object, GstObject *orig,
         else
             str = gst_value_serialize(&value);
 
-        if (g_strstr(GST_OBJECT_NAME(orig), "local-video-test-source-bin") != NULL) {
-          GST_INFO_OBJECT(orig, "foundsome\n");
-        }
         GST_INFO_OBJECT(object, "%s%s = %s\n", path, pspec->name, str);
         g_free(str);
         g_value_unset(&value);
